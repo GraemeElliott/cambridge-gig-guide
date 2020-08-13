3a30ad675e66fbb497b21f4e0be665cb8be08a8e
Graeme Elliott <graeme.elliott@dimedout.com>
2020-08-13 23:22:19 +0100

    Index page almost complete

--- assets/scripts/main-site-scripts.js
+++ assets/scripts/main-site-scripts.js
@@ -11,14 +11,14 @@ function closeNav() {
 
 
 $(document).ready(function(){
-  $('.gig-grid-x4').slick({
+  $('.gig-grid-nr').slick({
     dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    asNavFor: '.gig-panel',
+    asNavFor: '.gig-panel-nr',
     centerMode: true,
     centerPadding: '40px',
     focusOnSelect: true,
@@ -48,12 +48,57 @@ $(document).ready(function(){
       }
     ]
   });
-  $('.gig-panel').slick({
+  $('.gig-panel-nr').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.gig-grid-x4'
+    asNavFor: '.gig-grid-nr'
+});
+
+$('.gig-grid-ug').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  asNavFor: '.gig-panel-ug',
+  centerMode: true,
+  centerPadding: '40px',
+  focusOnSelect: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+$('.gig-panel-ug').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.gig-grid-ug'
 });
 });
 
