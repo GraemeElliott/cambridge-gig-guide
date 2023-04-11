b68789b3c46ddd830016d9708350dbb8c5ea865b
Graeme Elliott <graeme.elliott@dimedout.com>
2023-04-11 19:37:11 +0100

    Updated package.json file

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose'),
-      dotenv = require('dotenv');
+  dotenv = require('dotenv');
 
 process.on('uncaughtException', (error) => {
-  console.log ('Uncaught Exception. Shutting down...');
-  console.log (error.name, error.message);
+  console.log('Uncaught Exception. Shutting down...');
+  console.log(error.name, error.message);
   process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}).then(() => console.log ("DB connection successful"));
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 5500;
-const server = app.listen (port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on ('unhandledRejection', (error) => {
-  console.log (error.name, error.message);
-  console.log ('Unahndled Rejection. Shutting down...');
+process.on('unhandledRejection', (error) => {
+  console.log(error.name, error.message);
+  console.log('Unahndled Rejection. Shutting down...');
   server.close(() => {
     process.exit(1);
   });
