575555cb1d5278c8d974d6915d8f03dc9e230da5
Graeme Elliott <graeme.elliott@dimedout.com>
2023-04-11 19:55:52 +0100

    Updated server.js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose'),
-  dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 process.on('uncaughtException', (error) => {
   console.log('Uncaught Exception. Shutting down...');
@@ -8,10 +8,14 @@ process.on('uncaughtException', (error) => {
 });
 
 dotenv.config({ path: './config.env' });
+
+// Import the app module
 const app = require('./app');
 
+// Get the MongoDB URI from environment variables
 const DB = process.env.DATABASE;
 
+// Connect to MongoDB using Mongoose
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -19,16 +23,23 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful'));
+  .then(() => console.log('DB connection successful'))
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+  });
 
+// Get the port from environment variables or use a default value
 const port = process.env.PORT || 5500;
+
+// Start the server and listen for incoming requests
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (error) => {
   console.log(error.name, error.message);
-  console.log('Unahndled Rejection. Shutting down...');
+  console.log('Unhandled Rejection. Shutting down...');
   server.close(() => {
     process.exit(1);
   });
